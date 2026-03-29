6bdf7a38ccd6cca66c0ebb3058ce0d2178ad64a3
LeGiaBao2109 <giabao.yugioh@gmail.com>
2026-03-29 14:03:08 +0700

    Setup News List

--- src/routes/client/index.route.js
+++ src/routes/client/index.route.js
@@ -3,10 +3,12 @@ const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
 const promotionRoutes = require("./promotion.route");
 const authRoutes = require("./auth.route");
+const newsRoutes = require("./news.route");
 
 router.use('/', homeRoutes);
 router.use('/products', productRoutes);
 router.use('/promotions', promotionRoutes);
 router.use('/auth', authRoutes);
+router.use('/news', newsRoutes);
 
 module.exports = router;
