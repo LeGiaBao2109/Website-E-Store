54921bbab29e19edbb544dbbe735d125a2cb45a5
LeGiaBao2109 <giabao.yugioh@gmail.com>
2026-03-29 14:19:44 +0700

    Setup News Detail

--- src/controllers/client/news.controller.js
+++ src/controllers/client/news.controller.js
@@ -3,3 +3,7 @@ const path = require('path');
 module.exports.list = (req, res) => {
     res.sendFile(path.join(__dirname, "../../views/news.html"));
 };
+
+module.exports.detail = (req, res) => {
+    res.sendFile(path.join(__dirname, "../../views/news-detail.html"));
+};
--- src/routes/client/news.route.js
+++ src/routes/client/news.route.js
@@ -4,4 +4,6 @@ const newsController = require("../../controllers/client/news.controller");
 
 router.get('/', newsController.list)
 
+router.get('/detail', newsController.detail)
+
 module.exports = router;
