0cc9f3f55d9522c50bfce83a6364d1e19bab3b74
LeGiaBao2109 <giabao.yugioh@gmail.com>
2026-03-29 12:43:11 +0700

    Setup Login Form

--- src/routes/client/index.route.js
+++ src/routes/client/index.route.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
 const promotionRoutes = require("./promotion.route");
+const authRoutes = require("./auth.route");
 
 router.use('/', homeRoutes);
 router.use('/products', productRoutes);
 router.use('/promotions', promotionRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
